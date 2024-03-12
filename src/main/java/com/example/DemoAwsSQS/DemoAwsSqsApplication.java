e1132a968102ba7837d56648b282dcbd52fb8dce
Marina Pavlitskaya <marinabushneva@gmail.com>
2024-03-12 14:25:22 +0200

    structure+build.gradle+gradle.properties

---
+++ src/main/java/com/example/DemoAwsSQS/DemoAwsSqsApplication.java
@@ -0,0 +1,13 @@
+package com.example.DemoAwsSQS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAwsSqsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAwsSqsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DemoAwsSQS/DemoAwsSqsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DemoAwsSQS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAwsSqsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
